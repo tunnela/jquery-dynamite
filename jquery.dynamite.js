1e15f4049e80ab0f141bf9f9716ce5c01f307820
Lauri Tunnela <lauri@tunne.la>
2015-09-17 13:23:08 +0300

    Better pageReady event handling

--- jquery.dynamite.js
+++ jquery.dynamite.js
@@ -25,6 +25,8 @@
 		}
 		return $element;
 	},
+	instanceCount = 1,
+	instances = {},
 	linkId = 0,
 	links = {},
 	mergeAttributes = function($element, html, tag) {
@@ -104,80 +106,106 @@
 		$bodyScripts = $('script', $body).filter(isAnyScript),
 		$stylesheets = $(),
 		$scripts = $(),
-		render = function() {
-			$('head *').filter(isOther).remove();
+		options = {
+			bodyHTML: stripScripts($body.html()),
+			headStylesheets: function() {
+				$('head link').filter(isStylesheet).filter(function() {
+					var href = $(this).attr('href'),
+					$found = $headStylesheets.filter(function() {
+						return attributeMatches.call(this, 'href', href);
+					}),
+					result = $found.length === 0;
 
-			$('head link').filter(isStylesheet).filter(function() {
-				var href = $(this).attr('href'),
-				$found = $headStylesheets.filter(function() {
-					return attributeMatches.call(this, 'href', href);
-				}),
-				result = $found.length === 0;
+					$headStylesheets = $headStylesheets.filter(function() {
+						return attributeMatchesNot.call(this, 'href', href);
+					});
+					return result;
+				})
+				.remove();
 
-				$headStylesheets = $headStylesheets.filter(function() {
-					return attributeMatchesNot.call(this, 'href', href);
-				});
-				return result;
-			})
-			.remove();
+				if ($headStylesheets.length) {
+					$('head').append($headStylesheets);
+				}
+			},
+			headScripts: function() {
+				$('head script').remove();
 
-			$('head script').remove();
-			$('body script').remove();
+				if ($headScripts.length) {
+					$('head').append($headScripts);
+				}
+			},
+			headOthers: function() {
+				$('head *').filter(isOther).remove();
 
-			if ($headStylesheets.length) {
-				$('head').append($headStylesheets);
-			}
-			if ($headScripts.length) {
-				$('head').append($headScripts);
-			}
-			if ($headOthers.length) {
-				$('head').append($headOthers);
-			}
-			$('body').html(stripScripts($body.html()));
-			mergeAttributes($('body'), response, 'body');
+				if ($headOthers.length) {
+					$('head').append($headOthers);
+				}
+			},
+			bodyAttributes: function() {
+				mergeAttributes($('body'), response, 'body');
+			},
+			bodyScripts: function() {
+				$('body script').remove();
 
-			if ($bodyScripts.length) {
-				var pendingScripts = [];
-				var $bodyScript;
+				if ($bodyScripts.length) {
+					var pendingScripts = [];
+					var $bodyScript;
 
-				var loadScripts = function(exec) {
-					if ($bodyScripts.length <= 0) {
-						exec();
-						return;
-					}
-					var script;
-					var scriptEl = $bodyScripts.get(0);
-					var inline = typeof scriptEl.src === 'undefined' || !scriptEl.src;
+					var loadScripts = function(exec) {
+						if ($bodyScripts.length <= 0) {
+							exec();
+							return;
+						}
+						var script;
+						var scriptEl = $bodyScripts.get(0);
+						var inline = typeof scriptEl.src === 'undefined' || !scriptEl.src;
 
-					$bodyScripts.splice(0, 1);
+						$bodyScripts = $bodyScripts.slice(1);
 
-					script = document.createElement('script');
-
-					if (inline) {
+						script = document.createElement('script');
 						script.async = false;
-						script.innerHTML = scriptEl.innerHTML;
-						document.body.appendChild(script);
-						loadScripts(exec);
-					} else {
-						script.onload = function() {
+
+						if (inline) {
+							script.innerHTML = scriptEl.innerHTML;
+							document.body.appendChild(script);
 							loadScripts(exec);
-						};
-						script.async = false;
-						script.src = scriptEl.src;
-						document.body.appendChild(script);
-					}
-				};
+						} else {
+							script.onload = function() {
+								loadScripts(exec);
+							};
+							script.src = scriptEl.src;
+							document.body.appendChild(script);
+						}
+					};
 
-				loadScripts(function() {
-					$(window).trigger('load');
-				});
+					loadScripts(function() {
+						$(window).trigger('load');
+					});
+				}
+			},
+			replaceBody: function() {
+				$('body').html(this.bodyHTML);
+				this.bodyAttributes();
+			},
+			renderHead: function() {
+				this.headOthers();
+				this.headStylesheets();
+				this.headScripts();
+			},
+			renderBody: function() {
+				this.replaceBody();
+				this.bodyScripts();
+			},
+			render: function() {
+				this.renderHead();
+				this.renderBody();
 			}
 		};
 
-		$(this).one('pageReady', function() {
-			render();
+		$(this).one('pageReady', function(e, opts) {
+			opts.render();
 		})
-		.trigger('pageReady', [stripScripts($body.html()), render]);
+		.trigger('pageReady', [options]);
 	},
 	loadUrl = function(url) {
 		var that = this;
@@ -207,7 +235,19 @@
 			}
 		});
 	},
-	asyncSupport = 'async' in document.createElement('script');
+	asyncSupport = 'async' in document.createElement('script'),
+	onClick = function(e) {
+		var href = $(this).attr('href');
+
+		var a = document.createElement('a');
+		a.href = window.location.href;
+
+		if (href && this.host === a.host) {
+			e.preventDefault();
+			loadUrl.call(this, href);
+			return false;
+		}
+	};
 
 	function Dynamite(element, options) {
 		var $element = $(element),
@@ -216,34 +256,47 @@
 		options = $.extend({}, defaults, options);
 
 		this.options = options;
+		this.element = function() {
+			return element;
+		};
 
 		initDynamite();
 
-		$element.one('click', function(e) {
-			var href = $(this).attr('href');
-
-			var a = document.createElement('a');
-			a.href = window.location.href;
-
-			if (href && this.host === a.host) {
-				e.preventDefault();
-				loadUrl.call(this, href);
-				return false;
-			}
-		});
+		$element.on('click', onClick);
 	}
 
 	$.fn.dynamite = function(options) {
-		return this.each(function() {
+		var apis = [],
+		elements = this.each(function() {
 			if (!asyncSupport) {
 				return;
 			}
-			var dynamite = $(this).data('dynamite');
+			var id = $(this).data('dynamite'), instance = null;
 
-			if (!isDynamiteObject(dynamite)) {
-				$(this).data('dynamite', new Dynamite(this, options));
+			if (id) {
+				instance = instances[id] || null;
+
+				if (instance === null) {
+					return;
+				}
+			} else {
+				id = instanceCount++;
+				$(this).data('dynamite', id);
 			}
-		});
+			if (instance === null || typeof options !== 'undefined') {
+				instance = new Dynamite(this, options);
+				instances[id] = instance;
+			} else {
+				apis.push(instance);
+			}
+		}),
+		apiCount = apis.length;
+
+		if (apiCount) {
+			return apiCount === 1 ? apis[0] : apis;
+		} else {
+			return elements;
+		}
 	};
 
 	$(function() {
